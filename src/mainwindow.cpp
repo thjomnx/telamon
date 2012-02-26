9b266ad4c6cbfd62e3dcbdb98ca2672dd9f65da8
Markus Enax <Markus.Enax@gmail.com>
2012-02-26 20:09:03 +0100

    Basic actions added to main window

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,4 +6,37 @@ MainWindow::MainWindow()
     : QMainWindow()
 {
     setupUi(this);
+
+    createActions();
+    createMenus();
+
+    makeConnections();
+}
+
+void MainWindow::about()
+{
+    QString infoText = tr("There isn't so much yet ...");
+    QMessageBox::about(this, tr("About Telamon"), infoText);
+}
+
+void MainWindow::createActions()
+{
+    quitAction = new QAction(tr("&Quit"), this);
+    quitAction->setShortcut(QKeySequence::Quit);
+    quitAction->setStatusTip(tr("Terminate all sessions and quit the application"));
+
+    aboutAction = new QAction(tr("&About"), this);
+    aboutAction->setStatusTip(tr("Show some infos about this application"));
+}
+
+void MainWindow::createMenus()
+{
+    menu_File->addAction(quitAction);
+    menu_Help->addAction(aboutAction);
+}
+
+void MainWindow::makeConnections()
+{
+    connect(quitAction, SIGNAL(triggered(bool)), this, SLOT(close()));
+    connect(aboutAction, SIGNAL(triggered(bool)), this, SLOT(about()));
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -5,12 +5,26 @@
 
 #include "ui_mainwindow.h"
 
+class QAction;
+
 class MainWindow : public QMainWindow, public Ui::MainWindow
 {
     Q_OBJECT
 
 public:
     MainWindow();
+
+private slots:
+    void about();
+
+private:
+    void createActions();
+    void createMenus();
+
+    void makeConnections();
+
+    QAction *quitAction;
+    QAction *aboutAction;
 };
 
 #endif // MAINWINDOW_H
